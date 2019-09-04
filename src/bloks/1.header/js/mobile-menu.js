d306f46d46ecbbb5411ff94ab2c8a207eb40113c
Alex <49100406+alexmusyka@users.noreply.github.com>
2019-09-04 20:02:04 +0300

    Update mobile-menu.js

--- src/bloks/1.header/js/mobile-menu.js
+++ src/bloks/1.header/js/mobile-menu.js
@@ -1,17 +1,15 @@
+let headerMobileButton = document.getElementsByClassName(
+    "mobile-nav-button-container"
+  )[0],
+  mainLogo = document.getElementsByClassName("main-logo")[0],
+  mainNavList = document.getElementsByClassName("main-nav__list")[0];
 
-var headerMobileButton = document.getElementsByClassName("mobile-nav-button-container")[0];
-var mainLogo = document.getElementsByClassName("main-logo")[0];
-var mainNavList = document.getElementsByClassName("main-nav__list")[0];
-headerMobileButton.addEventListener("click", toggleClass);
-
-function toggleClass() {
-
+headerMobileButton.addEventListener("click", function() {
   if (mainLogo.className == "main-logo") {
-   mainLogo.className = "visually-hidden";
-    mainNavList.style.visibility = 'visible';
-  }
-  else {
+    mainLogo.className = "visually-hidden";
+    mainNavList.style.visibility = "visible";
+  } else {
     mainLogo.className = "main-logo";
-    mainNavList.style.visibility = 'hidden';
+    mainNavList.style.visibility = "hidden";
   }
-}
+});
