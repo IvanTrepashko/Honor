2c0c4e8e068be786fdc863f7a3f34d8cfab469eb
Alex <49100406+alexmusyka@users.noreply.github.com>
2019-09-04 20:02:45 +0300

    Update play-video.js

--- src/bloks/3.video/js/play-video.js
+++ src/bloks/3.video/js/play-video.js
@@ -1,23 +1,18 @@
-
-var text = document.getElementById("video-text");
-var button = document.getElementById("video-button");
-var video = document.getElementById("main-video");
-var honor = document.getElementById("honor-section");
-button.addEventListener("click", toggleClass);
-
-function toggleClass() {
-
+let text = document.getElementById("video-text"),
+  button = document.getElementById("video-button"),
+  video = document.getElementById("main-video"),
+  honor = document.getElementById("honor-section");
+button.addEventListener("click", function() {
   if (text.className == "container") {
     text.className = "visually-hidden";
     playClip(video);
     button.className = "visually-hidden";
-    honor.style.marginTop = '70px';
+    honor.style.marginTop = "70px";
     video.setAttribute("controls", "controls");
-  }
-  else {
+  } else {
     text.className = "container";
   }
-}
+});
 
 function playClip(media) {
   media.play();
