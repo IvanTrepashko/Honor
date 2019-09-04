e2c5b7fb10a242cd5ace6eae4bed90a9716d2947
Alex <49100406+alexmusyka@users.noreply.github.com>
2019-09-04 20:03:22 +0300

    Update skills.js

--- src/bloks/5.skills/js/skills.js
+++ src/bloks/5.skills/js/skills.js
@@ -1,11 +1,8 @@
-var block = document.getElementById("box-block");
-var skillsPhotoBlock = document.getElementById("skills-photo-block");
-var boxButton = document.getElementById("box-block-button");
-boxButton.addEventListener("click", CloseButton);
-
-
-function CloseButton() {
+let block = document.getElementById("box-block"),
+ skillsPhotoBlock = document.getElementById("skills-photo-block"),
+ boxButton = document.getElementById("box-block-button");
+boxButton.addEventListener("click", function(){
     block.className = "visually-hidden";
     skillsPhotoBlock.style.marginBottom = '0px';
     skillsPhotoBlock.style.marginRight = '170px';
-}
+});
